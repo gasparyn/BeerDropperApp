0fc5de8ff72ff1388642f6d41c93297f57ab61f7
gasparyn <kenyangas@gmail.com>
2011-12-10 14:08:30 -0500

    synched code with Jareds

--- src/com/BeerDropper/www/ManualSignActivity.java
+++ src/com/BeerDropper/www/ManualSignActivity.java
@@ -1,17 +1,5 @@
 /*
- * Copyright (C) 2007 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * BeerDropper(c)
  */
 
 package com.BeerDropper.www;
@@ -31,204 +19,189 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 
+/**
+ * Main activity that lets a customer append their signature to a delivery during a drop off.
+ * The signature is added to the customers record and send to the server for storage 
+ * 	
+ * @author gasparobimba
+ *
+ */
 public class ManualSignActivity extends GraphicsActivity
-        implements ColorPickerDialog.OnColorChangedListener {
+								implements ColorPickerDialog.OnColorChangedListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(new MyView(this));
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(new MyView(this));
 
-        mPaint = new Paint();
-        mPaint.setAntiAlias(true);
-        mPaint.setDither(true);
-        mPaint.setColor(0xFFFF0000);
-        mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setStrokeJoin(Paint.Join.ROUND);
-        mPaint.setStrokeCap(Paint.Cap.ROUND);
-        mPaint.setStrokeWidth(12);
+		mPaint = new Paint();
+		mPaint.setAntiAlias(true);
+		mPaint.setDither(true);
+		mPaint.setColor(0xFFFF0000);
+		mPaint.setStyle(Paint.Style.STROKE);
+		mPaint.setStrokeJoin(Paint.Join.ROUND);
+		mPaint.setStrokeCap(Paint.Cap.ROUND);
+		mPaint.setStrokeWidth(5);
+	}
 
-        mEmboss = new EmbossMaskFilter(new float[] { 1, 1, 1 },
-                                       0.4f, 6, 3.5f);
+	private Paint   mPaint;
+	private Path    mPath;
+	private Bitmap  mBitmap;
 
-        mBlur = new BlurMaskFilter(8, BlurMaskFilter.Blur.NORMAL);
-    }
+	public void colorChanged(int color) {
+		mPaint.setColor(color);
+	}
 
-    private Paint       mPaint;
-    private MaskFilter  mEmboss;
-    private MaskFilter  mBlur;
-    private static Bitmap  mBitmap;
+	public class MyView extends View {
 
-    public void colorChanged(int color) {
-        mPaint.setColor(color);
-    }
+		private static final float MINP = 0.25f;
+		private static final float MAXP = 0.75f;
 
-    public class MyView extends View {
 
-        private static final float MINP = 0.25f;
-        private static final float MAXP = 0.75f;
+		private Canvas  mCanvas;
+		private Paint   mBitmapPaint;
 
-      
-        private Canvas  mCanvas;
-        private Path    mPath;
-        private Paint   mBitmapPaint;
+		public MyView(Context c) {
+			super(c);
 
-        public MyView(Context c) {
-            super(c);
+			mPath = new Path();
+			mBitmapPaint = new Paint(Paint.DITHER_FLAG);
+		}
 
-            mPath = new Path();
-            mBitmapPaint = new Paint(Paint.DITHER_FLAG);
-        }
+		@Override
+		protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+			super.onSizeChanged(w, h, oldw, oldh);
+			mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+			mCanvas = new Canvas(mBitmap);
+		}
 
-        @Override
-        protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-            super.onSizeChanged(w, h, oldw, oldh);
-            mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-            mCanvas = new Canvas(mBitmap);
-        }
+		@Override
+		protected void onDraw(Canvas canvas) {
+			canvas.drawColor(0xFFAAAAAA);
 
-        @Override
-        protected void onDraw(Canvas canvas) {
-            canvas.drawColor(0xFFAAAAAA);
+			canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
 
-            canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
+			canvas.drawPath(mPath, mPaint);
+		}
 
-            canvas.drawPath(mPath, mPaint);
-        }
+		private float mX, mY;
+		private static final float TOUCH_TOLERANCE = 1;
 
-        private float mX, mY;
-        private static final float TOUCH_TOLERANCE = 4;
+		private void touch_start(float x, float y) {
+			mPath.reset();
+			mPath.moveTo(x, y);
+			mX = x;
+			mY = y;
+		}
+		private void touch_move(float x, float y) {
+			float dx = Math.abs(x - mX);
+			float dy = Math.abs(y - mY);
+			if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+				mPath.quadTo(mX, mY, (x + mX)/2, (y + mY)/2);
+				mX = x;
+				mY = y;
+			}
+		}
+		private void touch_up() {
+			mPath.lineTo(mX, mY);
+			// commit the path to our offscreen
+			mCanvas.drawPath(mPath, mPaint);
+			// kill this so we don't double draw
+			mPath.reset();
+		}
 
-        private void touch_start(float x, float y) {
-            mPath.reset();
-            mPath.moveTo(x, y);
-            mX = x;
-            mY = y;
-        }
-        private void touch_move(float x, float y) {
-            float dx = Math.abs(x - mX);
-            float dy = Math.abs(y - mY);
-            if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
-                mPath.quadTo(mX, mY, (x + mX)/2, (y + mY)/2);
-                mX = x;
-                mY = y;
-            }
-        }
-        private void touch_up() {
-            mPath.lineTo(mX, mY);
-            // commit the path to our offscreen
-            mCanvas.drawPath(mPath, mPaint);
-            // kill this so we don't double draw
-            mPath.reset();
-        }
 
-        @Override
-        public boolean onTouchEvent(MotionEvent event) {
-            float x = event.getX();
-            float y = event.getY();
+		@Override
+		public boolean onTouchEvent(MotionEvent event) {
+			float x = event.getX();
+			float y = event.getY();
 
-            switch (event.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-                    touch_start(x, y);
-                    invalidate();
-                    break;
-                case MotionEvent.ACTION_MOVE:
-                    touch_move(x, y);
-                    invalidate();
-                    break;
-                case MotionEvent.ACTION_UP:
-                    touch_up();
-                    invalidate();
-                    break;
-            }
-            return true;
-        }
-    }
+			switch (event.getAction()) {
+			case MotionEvent.ACTION_DOWN:
+				touch_start(x, y);
+				invalidate();
+				break;
+			case MotionEvent.ACTION_MOVE:
+				touch_move(x, y);
+				invalidate();
+				break;
+			case MotionEvent.ACTION_UP:
+				touch_up();
+				invalidate();
+				break;
+			}
+			return true;
+		}
+	}
 
-    private static final int COLOR_MENU_ID = Menu.FIRST;
-    private static final int EMBOSS_MENU_ID = Menu.FIRST + 1;
-    private static final int BLUR_MENU_ID = Menu.FIRST + 2;
-    private static final int ERASE_MENU_ID = Menu.FIRST + 3;
-    private static final int SRCATOP_MENU_ID = Menu.FIRST + 4;
-	private static final int SAVE_MENU_ID = Menu.FIRST+5;
+	private static final int COLOR_MENU_ID = Menu.FIRST;
+	private static final int ERASE_MENU_ID = Menu.FIRST + 1;
+	private static final int SAVE_MENU_ID = Menu.FIRST  + 2;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		menu.add(0, COLOR_MENU_ID, 0, "Color").setShortcut('3', 'c');
+		menu.add(0, ERASE_MENU_ID, 0, "Reset").setShortcut('5', 'z');
+		menu.add(0, SAVE_MENU_ID, 0, "Verify").setShortcut('5', 'z');
+		return true;
+	}
 
-        menu.add(0, COLOR_MENU_ID, 0, "Color").setShortcut('3', 'c');
-        menu.add(0, EMBOSS_MENU_ID, 0, "Emboss").setShortcut('4', 's');
-        menu.add(0, BLUR_MENU_ID, 0, "Blur").setShortcut('5', 'z');
-        menu.add(0, ERASE_MENU_ID, 0, "Erase").setShortcut('5', 'z');
-        menu.add(0, SRCATOP_MENU_ID, 0, "SrcATop").setShortcut('5', 'z');
-        menu.add(0, SAVE_MENU_ID, 0, "Save").setShortcut('5', 'z');
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		super.onPrepareOptionsMenu(menu);
+		return true;
+	}
 
-        /****   Is this the mechanism to extend with filter effects?
-        Intent intent = new Intent(null, getIntent().getData());
-        intent.addCategory(Intent.CATEGORY_ALTERNATIVE);
-        menu.addIntentOptions(
-                              Menu.ALTERNATIVE, 0,
-                              new ComponentName(this, NotesList.class),
-                              null, intent, 0, null);
-        *****/
-        return true;
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		mPaint.setXfermode(null);
+		mPaint.setAlpha(0xFF);
 
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        super.onPrepareOptionsMenu(menu);
-        return true;
-    }
+		switch (item.getItemId()) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        mPaint.setXfermode(null);
-        mPaint.setAlpha(0xFF);
+		case COLOR_MENU_ID:
+			new ColorPickerDialog(this, this, mPaint.getColor()).show();
+			return true;
 
-        switch (item.getItemId()) {
-            case COLOR_MENU_ID:
-                new ColorPickerDialog(this, this, mPaint.getColor()).show();
-                return true;
-            case EMBOSS_MENU_ID:
-                if (mPaint.getMaskFilter() != mEmboss) {
-                    mPaint.setMaskFilter(mEmboss);
-                } else {
-                    mPaint.setMaskFilter(null);
-                }
-                return true;
-            case BLUR_MENU_ID:
-                if (mPaint.getMaskFilter() != mBlur) {
-                    mPaint.setMaskFilter(mBlur);
-                } else {
-                    mPaint.setMaskFilter(null);
-                }
-                return true;
-            case ERASE_MENU_ID:
-                mPaint.setXfermode(new PorterDuffXfermode(
-                                                        PorterDuff.Mode.CLEAR));
-                return true;
-            case SRCATOP_MENU_ID:
-                mPaint.setXfermode(new PorterDuffXfermode(
-                                                    PorterDuff.Mode.SRC_ATOP));
-                mPaint.setAlpha(0x80);
-                return true;
-            case SAVE_MENU_ID:
-            	if (mBitmap != null) {
-            	    try {
-            	        String path = Environment.getExternalStorageDirectory().toString();
-            	        OutputStream fOut = null;
-            	        File file = new File(path, "screentest.jpg");
-            	        fOut = new FileOutputStream(file);
-            	        mBitmap.compress(Bitmap.CompressFormat.JPEG, 85, fOut);
-            	        fOut.flush();
-            	        fOut.close();
-            	        Log.e("ImagePath", "Image Path : " + MediaStore.Images.Media.insertImage( getContentResolver(), file.getAbsolutePath(), file.getName(), file.getName()));
-            	    }
-            	    catch (Exception e) {
-            	        e.printStackTrace();
-            	    }
-            	}
-        }
-        return super.onOptionsItemSelected(item);
-    }
+		case ERASE_MENU_ID:
+			mPath.reset();
+		//	this.invalidate();
+			clearSign();
+			return true;
+
+		case SAVE_MENU_ID:
+			if (mBitmap != null) {
+				try {
+					String path = Environment.getExternalStorageDirectory().toString();
+					OutputStream fOut = null;
+					File file = new File(path, "screentest.jpg");
+					fOut = new FileOutputStream(file);
+					mBitmap.compress(Bitmap.CompressFormat.JPEG, 85, fOut);
+					//For now we store image locally.
+					
+					//INSERT CODE to send image to server
+					fOut.flush();
+					fOut.close();
+					Log.e("ImagePath", "Image Path : " + MediaStore.Images.Media.insertImage( getContentResolver(), file.getAbsolutePath(), file.getName(), file.getName()));
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * method to clear bitMap
+	 */
+	public void clearSign(){
+
+			mBitmap = null;
+			mPath = null;
+			mBitmap = Bitmap.createBitmap(480, 480, Bitmap.Config.ARGB_8888);
+			mPath = new Path();
+
+		}
 }
--- src/com/BeerDropper/www/Photo.java
+++ src/com/BeerDropper/www/Photo.java
@@ -1,49 +1,84 @@
 package com.BeerDropper.www;
 
+
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
- 
+
 public class Photo extends Activity {
-  
- ImageView imageiewImageCaptured;
-  
-   /** Called when the activity is first created. */
-   @Override
-   public void onCreate(Bundle savedInstanceState) {
-       super.onCreate(savedInstanceState);
-       setContentView(R.layout.main);
-       Button buttonImageCapture = (Button)findViewById(R.id.captureimage);
-       imageiewImageCaptured = (ImageView)findViewById(R.id.imagecaptured);
-       
-       buttonImageCapture.setOnClickListener(buttonImageCaptureOnClickListener);
-   }
-   
-   Button.OnClickListener buttonImageCaptureOnClickListener= new Button.OnClickListener(){
- 
-	  @Override
-	  public void onClick(View arg0) {
-	   // TODO Auto-generated method stub
-	   Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-	   startActivityForResult(intent, 0);
-	    
-	  }};
- 
- @Override
- protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-  // TODO Auto-generated method stub
-  super.onActivityResult(requestCode, resultCode, data);
-   
-  if (resultCode == RESULT_OK)
-  {
-   Bundle extras = data.getExtras();
-   Bitmap bmp = (Bitmap) extras.get("data");
-   imageiewImageCaptured.setImageBitmap(bmp); 
-  }
-   
- }
+
+	ImageView imageCaptured;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		//Camera Button
+		Button buttonImageCapture = (Button)findViewById(R.id.get_image);
+		imageCaptured = (ImageView)findViewById(R.id.imagecaptured);
+		buttonImageCapture.setOnClickListener(camerabutton);
+
+		//Navigation Button
+		Button nav = (Button)findViewById(R.id.get_directions);
+		nav.setOnClickListener(navigation);
+
+		//Call Button
+		Button call = (Button)findViewById(R.id.get_call);
+		call.setOnClickListener(phone);
+
+
+	}
+
+	//Executes when the camera button is clicked
+	Button.OnClickListener camerabutton= new Button.OnClickListener(){
+		@Override
+		public void onClick(View arg0) {
+			Intent i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+			startActivityForResult(i, 0);
+
+		}
+	};
+
+	//This method saves the picture on the screen
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if (resultCode == RESULT_OK)
+		{
+			Bundle extras = data.getExtras();
+			Bitmap bmp = (Bitmap) extras.get("data");
+			imageCaptured.setImageBitmap(bmp);
+		}
+
+	}
+	//Executes when the navigation button is pressed
+	Button.OnClickListener navigation = new Button.OnClickListener(){
+		@Override
+		public void onClick(View arg0) {
+			Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("google.navigation:q=Boston+MA")); startActivity(i);
+		}
+	};
+	//Executes when the call button is pressed
+	Button.OnClickListener phone = new Button.OnClickListener(){
+		@Override
+		public void onClick(View arg0) {
+			Uri number = Uri.parse("tel:" + "6172493645");
+			Intent dial = new Intent(Intent.ACTION_DIAL, number);
+			startActivity(dial);
+			startActivity(Intent.createChooser(dial, "select dialer"));
+		}
+	};
 }
+
+
+
+
